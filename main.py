8cbcd2ebd65b2af9e2442202f7dcea5dc82ae966
Neuton Foo <neuton.foo@gmail.com>
2022-04-27 17:01:51 -0700

    Added Google Vision

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
-from enum import Enum
-from typing import List, Literal, Tuple
+import os
+from io import BytesIO
+from typing import List, Set, Tuple
+
 import cv2 as cv
-from PIL import Image
 import numpy as np
+from dotenv import load_dotenv
+from google.cloud import vision
+from PIL import Image
+
+load_dotenv()
+
+client = vision.ImageAnnotatorClient.from_service_account_json(
+    os.environ["GOOGLE_VISION_CREDS_JSON"]
+)
 
 # Shot change at 1178 ->1179
 class FrameUtil:
@@ -12,7 +22,6 @@ class FrameUtil:
         frame2: np.ndarray,
         compare_method: int = cv.HISTCMP_BHATTACHARYYA,
     ) -> float:
-
         # Convert it to HSV
         hsv_current_frame = cv.cvtColor(frame1, cv.COLOR_BGR2HSV)
         hsv_next_frame = cv.cvtColor(frame2, cv.COLOR_BGR2HSV)
@@ -29,12 +38,19 @@ class FrameUtil:
 
         return cv.compareHist(hist_img1, hist_img2, compare_method)
 
-    histogram_compare_methods = {cv.HISTCMP_BHATTACHARYYA: "Bhattacharyya distance"}
+    histogram_compare_methods = {
+        cv.HISTCMP_CORREL: "Correlation",
+        cv.HISTCMP_CHISQR: "Chi-Square",
+        cv.HISTCMP_INTERSECT: "Intersection",
+        cv.HISTCMP_BHATTACHARYYA: "Bhattacharyya distance",
+        # cv.HISTCMP_HELLINGER: "Bhattacharyya distance",
+        cv.HISTCMP_CHISQR_ALT: "Alternative Chi-Square",
+        cv.HISTCMP_KL_DIV: "Kullback-Leibler divergence",
+    }
 
-    @staticmethod
-    def get_histogram_comparison_method_name(compare_method: int):
-        return True
-        # return histogram_compare_methods[compare_method]
+    @classmethod
+    def get_histogram_comparison_method_name(cls, compare_method: int):
+        return cls.histogram_compare_methods[compare_method]
 
 
 class RGBVideo:
@@ -51,36 +67,45 @@ class RGBVideo:
             self.frames_buffer.shape[0] / (self.width * self.height * self.channels)
         )
 
-        self.frames_buffer = self.frames_buffer.reshape(
-            (self.number_of_frames, self.channels, self.height, self.width)
-        )
+        # Initialize shots as the whole video
+        self.shots: List[Tuple[int, int]] = [(0, self.number_of_frames - 1)]
 
         # Shape = (9000, 3, 270, 480)
+        self.frames_buffer = self.frames_buffer.reshape(
+            (self.number_of_frames, self.channels, self.height, self.width), order="c"
+        )
+        # print(self.frames_buffer.flags)
 
-        # Swap Channel and Width to get (9000, 270, 480, 3)
-        self.frames_buffer = np.swapaxes(self.frames_buffer, 1, 3)
-
-        # Swap Width and Height to get (9000, 480, 270, 3)
-        self.frames_buffer = np.swapaxes(self.frames_buffer, 1, 2)
+        # Need to move RGB channel to last axis
+        self.frames_buffer = np.moveaxis(self.frames_buffer, 1, 3)
 
         print(
             f"Loaded '{self.file_name}' - {self.number_of_frames} frames loaded into buffer"
         )
 
-    def dump_frames(self, output_folder="video_frames"):
-
+    def dump_frames(self, output_folder="video_frames") -> None:
         print(f"Dumping frames into '{output_folder}' folder")
 
         for frame_index, frame_buffer in enumerate(self.frames_buffer):
             im = Image.fromarray(frame_buffer)
             im.save(f"{output_folder}/{frame_index}.png")
 
-    def detect_shots(
+            if frame_index % 100 == 0:
+                print(f"--- At frame {frame_index}")
+
+        print("Frame dump completed")
+
+    def scan_for_shots(
         self,
         compare_method: int = cv.HISTCMP_BHATTACHARYYA,
         compare_threshold: float = 0.7,
-    ):
-        self.shots: List[Tuple[int, int]] = []
+    ) -> None:
+        print(
+            f"Detecting shots with '{FrameUtil.get_histogram_comparison_method_name(compare_method)}' with threshold {compare_threshold}"
+        )
+
+        # Clear shots array
+        self.shots.clear()
 
         shot_start_frame_index = 0
 
@@ -94,18 +119,114 @@ class RGBVideo:
 
             if compare_val > compare_threshold:
                 self.shots.append((shot_start_frame_index, frame_index))
-                shot_start_frame_index = frame_index
+                shot_start_frame_index = frame_index + 1
 
-                print(f"Frame {frame_index} -> {frame_index + 1} = {compare_val}")
+                print(f"--- Frame {frame_index} -> {frame_index + 1} = {compare_val}")
 
         self.shots.append((shot_start_frame_index, len(self.frames_buffer) - 1))
 
         print(self.shots)
 
+    def debug_override_shots(self, shots):
+        self.shots = shots
+
+    def scan_shots_for_logos(self, frame_skip=5):
+        img = Image.fromarray(self.frames_buffer[2137])
+
+        # img_base64 = base64.b64encode(data.tobytes())
+
+        buffer = BytesIO()
+        img.save(buffer, format="PNG")
+
+        image = vision.Image(content=buffer.getvalue())
+        response = client.logo_detection(image=image)
+        logos = response.logo_annotations
+        print("Logos:")
+
+        for logo in logos:
+            print(logo.description)
+
+        if response.error.message:
+            raise Exception(
+                "{}\nFor more info on error messages, check: "
+                "https://cloud.google.com/apis/design/errors".format(
+                    response.error.message
+                )
+            )
+
+        img.close()
+
+    def scan_shots_for_logos(self, frame_skip=100):
+
+        detected_logos: Set[str] = set()
+
+        buffer = BytesIO()
+        for frame_index in range(0, self.number_of_frames, frame_skip):
+            print(f"Checking frame {frame_index}")
+            img = Image.fromarray(self.frames_buffer[frame_index])
+            img.save(buffer, format="PNG")
+
+            image = vision.Image(content=buffer.getvalue())
+            response = client.logo_detection(image=image)
+            logos = response.logo_annotations
+
+            for logo in logos:
+                detected_logos.add(logo.description)
+
+            buffer.seek(0)
+            buffer.truncate()
+
+        print(detected_logos)
+
+        # for shot_index, shot in enumerate(self.shots):
+        #     shot_start_frame_index = shot[0]
+        #     shot_end_frame_index = shot[1]
+        #     for frame_index in range(
+        #         shot_start_frame_index, shot_end_frame_index, frame_skip
+        #     ):
+        #         print(f"Checking frame {frame_index}")
+        #         img = Image.fromarray(self.frames_buffer[frame_index])
+        #         buffer = BytesIO()
+        #         img.save(buffer, format="PNG")
+        #         img.save(f"test{frame_index}.png", format="PNG")
+        #         buffer.flush()
+        #         image = vision.Image(content=buffer.getvalue())
+        #         response = client.logo_detection(image=image)
+        #         logos = response.logo_annotations
+
+        #         if len(logos) > 0:
+        #             if "subway" not in logos:
+        #                 print(f"Shot {shot_index} has logos but Subway not in it")
+        #                 print(logos)
+        #                 break
+        #             else:
+        #                 print(f"Shot {shot_index} has Subway in it")
+        #                 print(logos)
+        #                 break
+
+        # print(enco)
+        # exit(0)
+
 
 def main():
+    pass
     rgb_video = RGBVideo("dataset-001/dataset/Videos/data_test1.rgb")
-    rgb_video.detect_shots()
+    # # rgb_video.dump_frames()
+    # rgb_video.scan_for_shots()
+    rgb_video.debug_override_shots(
+        [
+            (0, 1178),
+            (1179, 2399),
+            (2400, 2849),
+            (2850, 4349),
+            (4350, 5549),
+            (5550, 5924),
+            (5925, 5986),
+            (5987, 5999),
+            (6000, 8999),
+        ]
+    )
+    rgb_video.scan_shots_for_logos()
 
 
 if __name__ == "__main__":
--- test.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-test = np.arange(4 * 3 * 3)
-test2 = test.reshape(4, 3, 3)
-
-
-print(test2)
